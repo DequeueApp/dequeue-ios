3f5c084fd4fff9455385f418ec2e9c663cb3a3fd
Victor Quinn <mail@victorquinn.com>
2025-12-21 10:35:55 -0500

    Add fresh project

---
+++ Dequeue/DequeueTests/DequeueTests.swift
@@ -0,0 +1,16 @@
+//
+//  DequeueTests.swift
+//  DequeueTests
+//
+//  Created by Victor Quinn on 12/21/25.
+//
+
+import Testing
+
+struct DequeueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
